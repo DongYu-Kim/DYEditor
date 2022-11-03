31011ab7dd299b5895b0d856a549c4be430df56c
김동유 <ehddb2252@gmail.com>
2022-11-03 23:22:59 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# npmDYEditor
-DYEditor for npm
+# DYEditor
+DYEditor is custom made to use [CKEditor5](https://www.npmjs.com/package/ckeditor5) in React.
+By using this, you can use CKEditor5 more conveniently.   
+
+## Quick start
+> npm install DYEditor
+
+```javascript
+import DYEditor, {getData, uploadImages} from "@DYEditor";
+
+export default Component () => {
+    return <DYEditor 
+        data={_data} // _data is the initial value. data must be a string.
+        readOnly={_readOnly} // readOnly must be a boolean.
+        imageUploader={_imageUploader} // imageUploader should be null or a function that takes a file as input and a imageUrl as output.
+    />
+}
+```
---
+++ functions/index.js
@@ -0,0 +1,32 @@
+export function dataURLtoFile(dataurl, fileName) {
+  let arr = dataurl.split(','),
+  mime = arr[0].match(/:(.*?);/)[1],
+  bstr = atob(arr[1]), 
+  n = bstr.length, 
+  u8arr = new Uint8Array(n);
+
+  while(n--) u8arr[n] = bstr.charCodeAt(n);
+
+  return new File([u8arr], fileName, {type:mime});
+}
+
+export function isBase64Image(imgEl) {
+  const imgSrc = imgEl.src;
+  if(imgSrc.substr(0, 11) === "data:image/") {
+    const idx = imgSrc.indexOf(';');
+    if(idx === -1) return false;
+    if(imgSrc.substr(idx + 1, 6) === "base64") return true;
+  }
+  return false;
+}
+
+export function removeImageUploadElement(editor) {
+  for(const imgUpload of editor.ui.view.toolbar.element.getElementsByClassName('ck-file-dialog-button')) {
+      imgUpload.style.display ='None';
+  }
+}
+
+export function switchToReadMode(editor) {
+  editor.enableReadOnlyMode(editor.id);
+  editor.ui.view.toolbar.element.style.display = 'none';
+}
